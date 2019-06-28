8fb1e021b27e8e553ff708c17503b72e1ebafb99
Paulo Eduardo Santos de Sousa <eduardo.lwx@gmail.com>
2019-06-28 19:26:11 -0300

    Add the lib from branch lib

--- app/src/main/java/com/example/testmavelocalrep/MainActivity.kt
+++ app/src/main/java/com/example/testmavelocalrep/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.testmavelocalrep
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.myandroidlibrary.LibClass
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, LibClass.constant, Toast.LENGTH_LONG).show()
     }
 }
--- myandroidlibrary/src/main/java/com/example/myandroidlibrary/LibClass.kt
+++
@@ -1,4 +0,0 @@
-package com.example.myandroidlibrary
-
-class LibClass {
-}
