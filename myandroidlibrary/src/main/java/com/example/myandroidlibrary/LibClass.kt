2e246f5e4539352cf27f53137284efc93194dfb8
Paulo Eduardo Santos de Sousa <eduardo.lwx@gmail.com>
2019-06-28 19:07:59 -0300

    Version 1.1.0

--- myandroidlibrary/src/main/java/com/example/myandroidlibrary/LibClass.kt
+++ myandroidlibrary/src/main/java/com/example/myandroidlibrary/LibClass.kt
@@ -3,7 +3,7 @@ package com.example.myandroidlibrary
 class LibClass {
 
     companion object {
-        val constant = "LibClass_Constant 1.0.0"
+        val constant = "LibClass_Constant 1.1.0"
     }
 
 }
